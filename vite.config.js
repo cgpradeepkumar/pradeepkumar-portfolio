09b784a1a6b8f8b1dec94bdcd0f69c23bafa2d63
pradeepkumar <cgpradeepkumar@gmail.com>
2026-02-08 11:45:15 +0000

    Configure local HTTPS with basic-ssl plugin (dev only)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const plugins = [react()]
+
+  if (mode === 'development') {
+    plugins.push(basicSsl())
+  }
+
+  return {
+    plugins,
+  }
 })
